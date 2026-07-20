4eac60c0a83813b73b4b401d3a30f7ea7b7c77b1
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-20 13:01:30 -0300

    Update procedural_landscape.dart

--- lib/widgets/procedural_landscape.dart
+++ lib/widgets/procedural_landscape.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import '../shaders/landscape_params.dart';
 import '../shaders/landscape_shader_controller.dart';
 import 'procedural_landscape_painter.dart';
-import 'package:flutter/scheduler.dart';
-
 
 /// Widget "pronto para usar": carrega `landscape.frag`, mantém uma única
 /// instância de [ui.FragmentShader] viva (evita recompilar/realocar a cada
